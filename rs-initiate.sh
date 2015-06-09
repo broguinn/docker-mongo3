d9af4f82bde55e670634e1c2a8bd999c17c24aed
Braden O'Guinn <braden.oguinn@mutualmobile.com>
2015-06-09 13:34:08 -0500

    Updates to mongo
    Signed-off-by: Braden O'Guinn <braden.oguinn@mutualmobile.com>

--- rs-initiate.sh
+++ rs-initiate.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #MONGO=$(/mongodb/bin/mongod -f /mongo.conf --fork | grep "forked process" | sed s,forked\ process:,,g)
-/mongodb/bin/mongod -f /mongo.conf --fork
+mongod -f /mongo.conf --fork
 # MONGO=$(echo $OUTPUT | grep "forked process" | sed s,forked\ process:,,g)
 # ?echo $MONGO
 
 echo
 echo
 echo "waiting for mongo to start"
-until /mongodb/bin/mongo --eval "print(1);" > /dev/null 2>&1
+until mongo --eval "print(1);" > /dev/null 2>&1
 do
 	echo -n .
 	sleep 1
@@ -17,12 +17,12 @@ done
 echo
 echo
 echo "initiating rs"
-/mongodb/bin/mongo --eval 'printjson(rs.initiate({ _id: "rs0", members: [ { _id: 0, host: "localhost:27017" } ] }))' --quiet
+mongo --eval 'printjson(rs.initiate({ _id: "rs0", members: [ { _id: 0, host: "localhost:27017" } ] }))' --quiet
 
 echo
 echo
 echo "waiting for rs"
-until /mongodb/bin/mongo --eval "printjson(rs.isMaster())" | grep ismaster | grep true > /dev/null 2>&1
+until mongo --eval "printjson(rs.isMaster())" | grep ismaster | grep true > /dev/null 2>&1
 do
 	echo -n .
 	sleep 1
@@ -31,7 +31,7 @@ done
 echo
 echo
 echo "shutting down server"
-/mongodb/bin/mongo admin --eval 'db.shutdownServer({force: true});' --quiet
+mongo admin --eval 'db.shutdownServer({force: true});' --quiet
 sleep 1
 
 # echo "loading default data"
